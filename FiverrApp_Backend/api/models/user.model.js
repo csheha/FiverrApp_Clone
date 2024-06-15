85c814f927df11af798d90c82f0198a7684a07a7
csheha <chanudishehani22@gmail.com>
2024-06-16 02:43:16 +0530

    initial commit backend

---
+++ FiverrApp_Backend/api/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+{
+    username:{
+    type:String,
+    required:true,
+    unique:true,
+    },
+
+    email:{
+    type:String,
+    required:true,
+    },
+
+    password:{
+    type:String,
+    required:true,
+    },
+
+    img:{
+    type:String,
+    required:false,
+    },
+
+    desc:{
+    type:String,
+    required:false,
+    },
+
+    isSellar:{
+    type:Boolean,
+    required:false,    
+    }
+
+},{
+    timestamps:true
+});
+
+export default mongoose.model("User",UserSchema);
